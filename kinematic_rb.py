798dcdbb88c568e54a0dba90efa5834eab4d393f
theanh2507 <85270925+theanh2507@users.noreply.github.com>
2025-04-10 14:28:49 +0700

    Update kinematic_rb.py

--- kinematic_rb.py
+++ kinematic_rb.py
@@ -17,6 +17,9 @@ class MotorControlNode(Node):
         self.encoder_pulse = 1000
         self.ratio = 0.5
 
+        self.max_rad_vel = 4.1666667                        # linear.x = 0.5
+        self.analog_max = 2000
+
         # Modbus parameters
         self.declare_parameter("port", "/dev/ttyUSB0")
         self.declare_parameter("baudrate", 9600)
@@ -44,31 +47,59 @@ class MotorControlNode(Node):
         # Nhận lệnh từ /cmd_vel
         self.create_subscription(Twist, "/cmd_vel", self.cmd_vel_callback, 10)
 
+    
+    def map_rad_s_to_analog(self, rad_s, max_rad_vel, analog_max):
+        # Giới hạn rad_s trong [-max_rad_s, max_rad_s]
+        rad_s = max(min(rad_s, max_rad_vel), -max_rad_vel)
+
+        analog_value = int((rad_s / max_rad_vel) * analog_max)
+
+        return analog_value
+
     def cmd_vel_callback(self, msg: Twist):
         v = msg.linear.x
         w = msg.angular.z
 
-        # Tính vận tốc bánh (m/s)
+        # van toc banh (m/s)
         v_r = v + (w * self.distance_two_wheel / 2)
         v_l = v - (w * self.distance_two_wheel / 2)
 
-        # Chuyển sang tốc độ góc (rad/s), rồi sang xung/chu kỳ
-        angular_r = v_r / self.radius_wheel
+        # van toc goc banh
+        angular_r = v_r / self.radius_wheel             # (rad/s)
         angular_l = v_l / self.radius_wheel
 
-        angular_r_rpm = (angular_r * 60) / (2 * pi)
-        angular_l_rpm = (angular_l * 60) / (2 * pi)
+        analog_r = self.map_rad_s_to_analog(angular_r, self.max_rad_vel, self.analog_max)
+        analog_l = self.map_rad_s_to_analog(angular_l, self.max_rad_vel, self.analog_max)
 
-        pulses_r = int((angular_r * self.encoder_pulse) / (2 * pi * self.ratio))
-        pulses_l = int((angular_l * self.encoder_pulse) / (2 * pi * self.ratio))
+        angular_r_rpm = int((angular_r * 60) / (2 * pi))
+        angular_l_rpm = int((angular_l * 60) / (2 * pi))
 
-        self.send_to_motor(angular_r_rpm, angular_l_rpm)
+        print(analog_r, analog_l)
 
-    def send_to_motor(self, angular_r_rpm, angular_l_rpm):
+  
+        self.send_to_motor(analog_r, analog_l)
+
+
+    def send_to_motor(self, analog_r, analog_l):
         try:
-            self.client.write_register(0, ctypes.c_int16(angular_r_rpm).value, slave=self.slave_id)  # banh phai
-            self.client.write_register(2, ctypes.c_int16(angular_l_rpm).value, slave=self.slave_id)  # banh trai
-            self.get_logger().info(f"Send Vel - L: {angular_l_rpm}, R: {angular_r_rpm}")
+            self.client.write_coil(address=3, value=True, slave=self.slave_id)
+
+            # Chiều bánh phải (M4)
+            if analog_r >= 0:
+                self.client.write_coil(address=4, value=False, slave=self.slave_id)  # Đi tới
+            else:
+                self.client.write_coil(address=4, value=True, slave=self.slave_id)   # Đi lùi
+
+            # Chiều bánh trái (M5)
+            if analog_l >= 0:
+                self.client.write_coil(address=5, value=True, slave=self.slave_id)   # Đi tới
+            else:
+                self.client.write_coil(address=5, value=False, slave=self.slave_id)  # Đi lùi
+
+            self.client.write_register(101, ctypes.c_int16(abs(analog_r)).value, slave=self.slave_id)  # banh phai
+            self.client.write_register(100, ctypes.c_int16(abs(analog_l)).value, slave=self.slave_id)  # banh trai
+            self.get_logger().info(f"Send Vel - L: {analog_l}, R: {analog_r}")
+        
         except Exception as e:
             self.get_logger().error(f"Error: {e}")
 
